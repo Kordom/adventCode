d593f73d45fb0e3f897ebad55efaefcc19838f11
Kordom1029 <artemij.sakovic@gmail.com>
2024-12-11 11:46:47 +0200

    small changes

--- day1.py
+++ day1.py
@@ -13,34 +13,34 @@ list2 = []
 distance = []
 
 if r.status_code == 200:
-    content = r.text.splitlines()  # Split by lines, for example
+    content = r.text.splitlines()
     for elem in content:
         items = elem.split()
         list1.append(int(items[0]))
         list2.append(int(items[1]))
-    print(list1)
-    print(list2)
+
+    for i in range(len(list1)):
+        min_number_in_list1 = min(list1)
+        min_number_index = list1.index(min_number_in_list1)
+
+        min_number_in_list2 = min(list2)
+        min_number_index2 = list2.index(min_number_in_list2)
+
+        if min_number_in_list1 > min_number_in_list2:
+            distance_value = min_number_in_list1 - min_number_in_list2
+            distance.append(distance_value)
+            list1.pop(min_number_index)
+            list2.pop(min_number_index2)
+        else:
+            distance_value = min_number_in_list2 - min_number_in_list1
+            distance.append(distance_value)
+            list1.pop(min_number_index)
+            list2.pop(min_number_index2)
+
+    print(sum(distance))
 else:
     print('None')
 
-for i in range(len(list1)):
-    min_number_in_list1 = min(list1)
-    min_number_index = list1.index(min_number_in_list1)
 
-    min_number_in_list2 = min(list2)
-    min_number_index2 = list2.index(min_number_in_list2)
-
-    if min_number_in_list1 > min_number_in_list2:
-        distance_value = min_number_in_list1 - min_number_in_list2
-        distance.append(distance_value)
-        list1.pop(min_number_index)
-        list2.pop(min_number_index2)
-    else:
-        distance_value = min_number_in_list2 - min_number_in_list1
-        distance.append(distance_value)
-        list1.pop(min_number_index)
-        list2.pop(min_number_index2)
-
-print(sum(distance))
 
 
